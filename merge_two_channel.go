b7c736d0fd9dd06a3541320d92768f5c6dfd88b0
lampScript <xzy19950128@163.com>
2019-08-22 14:50:10 +0800

    add .

---
+++ merge_two_channel.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	even := make(chan int)
+	odd := make(chan int)
+
+	go func() {
+		for i:=0;i <100 ;i++  {
+			if i % 2 == 0 {
+				even <- i
+			}
+		}
+		close(even)
+	}()
+
+	go func() {
+		for i:=0;i <100 ;i++  {
+			if i % 2 != 0 {
+				odd <- i
+			}
+		}
+		close(odd)
+	}()
+
+	for odd := range Merge(odd, even){
+		fmt.Println(odd)
+	}
+
+}
+
+func Merge(ch1 <-chan int, ch2 <-chan int) chan int {
+
+	out := make(chan int)
+
+	go func() {
+		// 等上游的数据 （这里有阻塞，和常规的阻塞队列并无不同）
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+
+		// 取数据
+		for ok1 || ok2 {
+			if !ok2 || (ok1 && v1 <= v2) {
+				// 取到最小值, 就推到 out 中
+				out <- v1
+				v1, ok1 = <-ch1
+			} else {
+				out <- v2
+				v2, ok2 = <-ch2
+			}
+		}
+		// 显式关闭
+		close(out)
+	}()
+
+	// 开完goroutine后, 主线程继续执行, 不会阻塞
+	return out
+}
