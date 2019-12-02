ab989ffa5eb6976c19b2d7c596326bed2f2116da
lampScript <xzy19950128@163.com>
2019-12-03 00:12:38 +0800

    add pow

---
+++ pow.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(myPow(2.0, -3))
+}
+
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		x = 1/x
+		n = -n
+	}
+
+	return fastPow(x, n)
+}
+
+func fastPow(x float64, n int) float64  {
+	if n == 0 {
+		return 1.0
+	}
+
+	half := fastPow(x, n/2)
+	if n%2== 0{
+		return half * half
+	} else {
+		return half * half * x
+	}
+}
